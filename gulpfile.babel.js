b42edfb7835e705a97ddb28873b40270d2b9489e
Kirill Baykov <kbaykov@ncem.ru>
2017-10-01 20:27:36 +0300

    Add book slider in header

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,11 +7,13 @@ import postcssimport from 'postcss-import';
 import sourcemaps from 'gulp-sourcemaps';
 import simplevars from 'postcss-simple-vars';
 import autoprefixer from 'autoprefixer';
+import mixins from 'postcss-mixins';
 import nested from 'postcss-nested';
 import csso from 'postcss-csso';
 import image from 'gulp-image';
 import watch from 'gulp-watch';
 import browsersync from 'browser-sync';
+import concat from 'gulp-concat';
 
 const path = {
 
@@ -20,13 +22,15 @@ const path = {
       css:    'build/css/',
       js:     'build/js/',
       img:    'build/img/',
+      fonts:   'build/fonts/'
    },
 
    src: {
       html:   'src/*.html',
       css:    'src/css/main.css',
-      js:     'src/js/bundle.js',
+      js:     'src/js/script.js',
       img:    'src/img/**/*.*',
+      fonts: 'src/fonts/**/*.*'
    },
 
    watch: {
@@ -34,6 +38,7 @@ const path = {
       css:    'src/css/**/*.css',
       js:     'src/js/**/*.js',
       img:    'src/img/**/*.*',
+      fonts: 'src/fonts/**/*.*'
    }
 };
 
@@ -61,6 +66,7 @@ gulp.task('build:css', () => {
       .pipe( sourcemaps.init() )
       .pipe( postcss([
          postcssimport,
+         mixins,
          simplevars,
          nested,
          autoprefixer,
@@ -74,20 +80,33 @@ gulp.task('build:css', () => {
 });
 
 gulp.task('build:js', () => {
-   gulp.src(path.src.js)
+    gulp.src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/owl.carousel/dist/owl.carousel.min.js',
+            path.src.js
+        ])
 		.pipe(babel({
 			presets: ['es2015']
 		}))
-      .pipe( gulp.dest(path.build.js) )
-      .pipe( browsersync.reload({
-         stream: true
-      }));
+        .pipe( concat('bundle.js') )
+        .pipe( gulp.dest(path.build.js) )
+        .pipe( browsersync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('build:img', () => {
    gulp.src(path.src.img)
       .pipe( image() )
       .pipe( gulp.dest(path.build.img) )
+      .pipe( browsersync.reload({
+         stream: true
+      }));
+});
+
+gulp.task('build:fonts', () => {
+   gulp.src(path.src.fonts)
+      .pipe( gulp.dest(path.build.fonts) )
       .pipe( browsersync.reload({
          stream: true
       }));
@@ -109,9 +128,13 @@ gulp.task('watch', () => {
    watch([path.watch.img], () => {
       gulp.start('build:img');
    });
+
+   watch([path.watch.fonts], () => {
+      gulp.start('build:fonts');
+   });
 });
 
-gulp.task('build', ['build:html', 'build:css', 'build:js', 'build:img']);
+gulp.task('build', ['build:html', 'build:css', 'build:js', 'build:img', 'build:fonts']);
 
 gulp.task('server', () => {
    browsersync(serverConfig);
---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+		loop:true,
+		smartSpeed: 1000,
+		nav:true,
+		dots:true,
+		dotsEach:true,
+		navText:false,
+		items: 1,
+		autoplay: false,
+	});
+});
