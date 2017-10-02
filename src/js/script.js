51537f0b5161a9362406d1204ca3d2736180b101
Kirill Baykov <kbaykov@ncem.ru>
2017-10-02 10:43:04 +0300

    Add social block

--- src/js/script.js
+++ src/js/script.js
@@ -19,4 +19,24 @@ $(document).ready(function() {
 		items: 1,
 		autoplay: true,
 	});
+    $('.block_meeting').owlCarousel({
+		loop:true,
+		smartSpeed: 1000,
+		nav:true,
+		dots:true,
+		dotsEach:true,
+		navText:false,
+		items: 1,
+		autoplay: false,
+	});
+    $('.block_congresses').owlCarousel({
+		loop:true,
+		smartSpeed: 1000,
+		nav:true,
+		dots:true,
+		dotsEach:true,
+		navText:false,
+		items: 1,
+		autoplay: false,
+	});
 });
